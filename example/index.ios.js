bbf689d30bfa907fe7d4598a795156ab8ff05c44
Ran Greenberg <rang@wix.com>
2016-05-22 11:39:36 +0300

    update example

--- example/index.ios.js
+++ example/index.ios.js
@@ -25,23 +25,23 @@ class example extends Component {
         <View style={{ height: 300}}>
           <CustomSegmentedControl style={{
                                           flex:1,
-                                          backgroundColor: 'transparent'
-                                          }}
+                                          backgroundColor: '#D4D4D4',
+                                          marginVertical: 8
+                                        }}
                                   textValues={['ORDERS','HOTELS' ]}
                                   selected={0}
                                   segmentedStyle={{
                                                    lineSelectedHeight: 1.5,
-                                                   fontSize:16,
+                                                   fontSize:17,
                                                    segmentBackgroundColor: 'transparent',
                                                    segmentTextColor: 'black',
-                                                   segmentFontFamily: 'Cochin',
                                                    lineColor: 'black',
                                                    selectedLineAlign: 'bottom', // top/bottom/text
-                                                   selectedLineMode: 'text' // full/text
+                                                   selectedLineMode: 'full' // full/text
                                                  }}
                                   animation={{
-                                              duration: 0.4,
-                                              damping: 0.6
+                                              duration: 0.6,
+                                              damping: 0.7
                                               }}
                                   onSelectedWillChange={(event)=> {
                                   }}
@@ -50,46 +50,80 @@ class example extends Component {
 
           />
 
-          <CustomSegmentedControl style={{flex: 1, backgroundColor: 'green'}}
+          <CustomSegmentedControl style={{flex: 1, backgroundColor: '#2C82C9'}}
                                   selected={2}
                                   segmentedStyle={{
-                                         lineSelectedHeight: 1,
-                                         fontSize:12,
+                                         lineSelectedHeight: 2,
+                                         fontSize:20,
                                          segmentBackgroundColor: 'transparent',
                                          segmentTextColor: 'black',
-                                         segmentFontFamily: 'Cochin',
-                                         lineColor: 'black'
+                                         segmentFontFamily: 'Copperplate',
+                                         lineColor: '#8A2D3C',
                                          }}
+                                  animation={{
+                                              duration: 0.7,
+                                              damping: 0.2
+                                              }}
                                   textValues={['ORDERS','HOTELS', 'OPTIONS']}
           />
 
           <CustomSegmentedControl style={{flex: 1,
                                           backgroundColor: '#83D6DE',
-                                          //marginTop: 40,
                                           borderRadius: 20,
+                                          marginVertical: 8,
+                                          marginHorizontal: 4
                                          }}
                                   selected={1}
                                   segmentedStyle={{
                                                  lineSelectedHeight: 1,
-                                                 fontSize:12,
+                                                 fontSize:15,
                                                  segmentBackgroundColor: 'transparent',
                                                  segmentTextColor: 'blue',
-                                                 segmentFontFamily: 'Cochin',
+                                                 segmentFontFamily: 'Menlo',
                                                  lineColor: 'red'
                                                  }}
+                                  animation={{
+                                              duration: 0.3,
+                                              damping: 0.8
+                                              }}
                                   textValues={['TOP FREE','TOP PAID', 'TOP GROSSING']}
           />
 
-          <CustomSegmentedControl style={{ flex: 1,backgroundColor: '#3C3741'}}
+          <CustomSegmentedControl style={{ flex: 1,backgroundColor: '#3C3741', marginVertical: 8}}
                                   segmentedStyle={{
-
-                                                  lineSelectedHeight: 1,
+                                                  lineSelectedHeight: 3,
                                                   fontSize:12,
                                                   segmentBackgroundColor: 'transparent',
                                                   segmentTextColor: '#E0E4CC',
-                                                  segmentFontFamily: 'Cochin',
-                                                  lineColor: '#E0E4CC'
+                                                  segmentFontFamily: 'Verdana',
+                                                  lineColor: '#EB9532',
+                                                 selectedLineAlign: 'top'
                                                  }}
+                                  animation={{
+                                              duration: 0.3,
+                                              damping: 0.4
+                                              }}
+                                  textValues={['ORDERS','HOTELS', 'OPTIONS','TOP FREE','TOP PAID']}
+          />
+
+          <CustomSegmentedControl style={{ flex: 1,
+                                           backgroundColor: '#EFE0B1',
+                                           marginVertical: 8,
+                                           marginHorizontal: 4,
+                                        }}
+                                  segmentedStyle={{
+                                                  lineSelectedHeight: 3,
+                                                  fontSize:12,
+                                                  segmentBackgroundColor: 'transparent',
+                                                  segmentTextColor: '#60646D',
+                                                  lineColor: '#953163',
+                                                  selectedLineAlign: 'top',
+                                                  selectedLineMode: 'full'
+                                                 }}
+                                  animation={{
+                                              duration: 0.5,
+                                              damping: 0.6
+                                              }}
                                   textValues={['ORDERS','HOTELS', 'OPTIONS','TOP FREE','TOP PAID']}
           />
 
